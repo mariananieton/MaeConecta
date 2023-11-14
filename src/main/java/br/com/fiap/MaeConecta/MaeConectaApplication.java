4184fe25337d0fc1006aa26ab4c6d009d7fa6d3c
mariananieton <rm94141@fiap.com.br>
2023-11-14 10:12:20 -0300

    Initial commit

---
+++ src/main/java/br/com/fiap/MaeConecta/MaeConectaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.MaeConecta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaeConectaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaeConectaApplication.class, args);
+	}
+
+}
